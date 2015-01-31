3a0ce197da02289536c2451a1cfbfacd955c384d
bridgetlane <bridgetlane.mail@gmail.com>
2015-01-30 21:48:01 -0500

    working changes

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
         // Load images
